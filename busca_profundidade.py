a4104b52c98fb81c728baa136de88823f6a761ef
MarileneGarcia <marilenegarcia1998@gmail.com>
2020-05-17 18:36:12 -0300

    prints no terminal

---
+++ busca_profundidade.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Objetivo: Realizar a busca em profundiade na matriz de dado 
+    Variaveis de entrada: Matriz dos dados
+    Variaveis de saida: Lista de tuplas com o caminho do algoritmo """
+
+def busca_profundidade(matriz):
+  
--- trabalho_pratico.py
+++ trabalho_pratico.py
@@ -1,42 +1,70 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+""" O objetivo do codigo da funcao principal 
+e salvar os dados do arquivo de entrada em 
+uma matriz de strings, e chamar os arquivos 
+que vão executar os algoritmos """
+
 import sys
+from termcolor import colored
 
 def main( ) :
-  arquivo_entrada = sys.argv[1] 
-  #arquivo_saida = sys.argv[2]
-  
   matriz = []
-  arquivo = open(arquivo_entrada, 'r')
+  arquivo = open(sys.argv[1], 'r')
   
+  """ Leitura do numero de linhas e colunas """
   primeira_linha = arquivo.readline()
-  
   flag = 0
   n_linha = ""
   n_coluna = ""
   for i in primeira_linha:
     if(i != " " and flag == 0):
       n_linha = n_linha + i
-    
     elif (i != " " and flag >= 1):
-      n_coluna = n_coluna + i
-
-    #Aperfeicoar o erro ou tirar com a justificativa de deveria haver outro codigo so com essa funcao
-    #elif (i != " " and flag > 1):
-      #raise Exception("Arquivo fora de formato, envie apenas duas dimensões para a matriz") 
-    
+      n_coluna = n_coluna + i  
     else:
-      flag = flag + 1
+      flag += 1
+  n_linha = int(n_linha)
+  n_coluna = int(n_coluna)
+  
+  for a in range(int(n_coluna/2) - 5):
+    print("", end = " ")
+  print("LABIRINTO")
 
-  ##colocar uma verificacao de erro aq
+  """ Matriz com os dados do arquivo fornecido """  
   for linha in arquivo:
     lista = []
-    for coluna in linha :
-      lista.append(coluna)
+    for coluna in linha : 
+      if (coluna == "*"):
+        text = colored("#", "white")
+        print (text, end = '')
+        lista.append(0)
+      elif(coluna == "-"):
+        text = colored("#", "grey")
+        print (text, end = '')
+        lista.append(1)
+      elif(coluna == "#"):
+        text = colored("#", "blue")
+        print (text, end = '')
+        lista.append(2)
+      elif(coluna == "$"):
+        text = colored("#", "red")
+        print (text, end = '')
+        lista.append(3)
+    print("")
     matriz.append(lista)
-
-  print(matriz)
   arquivo.close()
+
+  """ Escolher o algoritmo a ser executado """
+  print("\nOs algoritmos que podem realizar a busca pelos caminhos do labirinto são: ")
+  print( "[ 0 ] Busca em profundidade")
+  print( "[ 1 ] Busca em largura")
+  print( "[ 2 ] Busca Best-First Search")
+  print( "[ 3 ] Busca A*")
+  print( "[ 3 ] Hill Climbing \n")
+  valor_lido = input("Digite o algortimo desejado e pressione 'enter': ")
+  
+
 
 
 if __name__ == "__main__":
